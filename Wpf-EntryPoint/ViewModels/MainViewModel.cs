7483355de32a82c20c7a2ebda637f4137f74e3c5
Sebastian Sokol <s.sokol@8mila.com>
2024-08-23 17:31:10 +0200

    Inizio +39

--- Wpf-EntryPoint/ViewModels/MainViewModel.cs
+++ Wpf-EntryPoint/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows.Input;
+using Wpf_EntryPoint.Views;
 
 namespace Wpf_EntryPoint.ViewModels
 {
@@ -24,11 +25,13 @@ namespace Wpf_EntryPoint.ViewModels
 
             ShowNuovaListaCommand = new RelayCommand(ShowNuovaLista);
             ShowNRICommand = new RelayCommand(ShowNRI);
+            ShowPulizia39Command = new RelayCommand(ShowPulizia39);
 
         }
 
         public ICommand ShowNuovaListaCommand { get; }
         public ICommand ShowNRICommand { get; }
+        public ICommand ShowPulizia39Command { get; }
 
         private void ShowNuovaLista()
         {
@@ -39,6 +42,10 @@ namespace Wpf_EntryPoint.ViewModels
             CurrentView = new SmsMassivi_Rni_ViewModel();
         }
 
+        private void ShowPulizia39()
+        {
+            CurrentView = new PuliziaNumeri_39_UserController();
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
---
+++ Wpf-EntryPoint/Views/PuliziaNumeri_39_UserController.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_EntryPoint.Views
+{
+    /// <summary>
+    /// Interaction logic for PuliziaNumeri_39_UserController.xaml
+    /// </summary>
+    public partial class PuliziaNumeri_39_UserController : UserControl
+    {
+        public PuliziaNumeri_39_UserController()
+        {
+            InitializeComponent();
+            this.DataContext = this.DataContext ?? Application.Current.MainWindow?.DataContext;
+        }
+    }
+}
